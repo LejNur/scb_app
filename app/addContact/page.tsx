b71e62af3ec7c123f5f1faa2555cf03e0fe0030a
Lejla <lejla.nrk@gmail.com>
2024-11-12 00:55:57 +0100

    style&chore: cleaning template/ layout/edit page

---
+++ app/addContact/page.tsx
@@ -0,0 +1,12 @@
+import Form from "@/components/Molecules/Form";
+
+function AddContact() {
+  return (
+    <div>
+      <h1>Add new contact</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default AddContact;
