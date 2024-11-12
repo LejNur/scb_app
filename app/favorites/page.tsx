cf25b0999eb2f49845adbfd67f5e85ce8423f42d
Lejla <lejla.nrk@gmail.com>
2024-11-12 11:16:02 +0100

    feat: loading page/tailwid animation

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,15 +1,16 @@
 import ContactsList from "@/components/Molecules/ContactList";
 import { getFavorites } from "../actions/getFavorites";
 
-async function Home() {
+async function Favorites() {
   const favorites = await getFavorites();
 
   return (
     <>
       <h1>Contact List</h1>
+
       <ContactsList contacts={favorites}></ContactsList>
     </>
   );
 }
 
-export default Home;
+export default Favorites;
