dfa78f2bd1379276c490ecff1b0bbb1011bd157f
Lejla <lejla.nrk@gmail.com>
2024-11-13 11:31:49 +0100

    style: navbar

--- components/Molecules/Navbar.tsx
+++ components/Molecules/Navbar.tsx
@@ -1,18 +1,30 @@
 "use client";
 import Link from "next/link";
+import PlusIcon from "../icons/PlusIcon";
+import HeartIcon from "../icons/HeartIcon";
 
 function Navbar() {
   return (
-    <nav className="bg-slate-700 w-full">
-      <Link className="text-white pr-3" href={"/"}>
-        All Contacts
-      </Link>
-      <Link className="text-white pr-3" href={"/favorites"}>
-        Favorites
-      </Link>
-      <Link className="text-white pr-3" href={"/addContact"}>
-        Add new contact
-      </Link>
+    <nav className="bg-slate-700 w-full py-6">
+      <div className="text-white flex justify-between align-baseline items-center mx-4 md:mx-8 lg:mx-10">
+        <h1 className="text-3xl">ContactsBook</h1>
+        <Link
+          className="text-blue-500 bg-slate-200 rounded-md px-2 flex gap-1 justify-center align-middle items-center"
+          href={"/addContact"}
+        >
+          <PlusIcon />
+          ADD
+        </Link>
+      </div>
+      <div className="flex justify-center mt-8 mb-2 justify-items-center align-baseline gap-6">
+        <Link className="text-white pr-3" href={"/"}>
+          All Contacts
+        </Link>
+        <Link className="text-white pr-3 flex gap-1" href={"/favorites"}>
+          Favorites
+          <HeartIcon className="size-6" fill="none" />
+        </Link>
+      </div>
     </nav>
   );
 }
