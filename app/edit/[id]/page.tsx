117e7066d6e9763774e63cb0c0f4a7d350c253f2
Lejla <lejla.nrk@gmail.com>
2024-11-14 23:20:25 +0100

    fix: vercel error

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -2,34 +2,9 @@ import { getContact } from "@/app/actions/getContact";
 import Form from "@/components/Molecules/Form";
 import React from "react";
 
-interface Props {
-  params: { id: string };
-}
-
-async function EditContact({ params }: Props) {
-  const { id } = await params;
+async function EditContact({ params }: any) {
+  const { id } = params;
   const contact = await getContact(id);
-
-  // const params = useParams<{ tag: string; id: string }>();
-  // const id = params.id;
-
-  // const [contact, setContact] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchContact = async () => {
-  //     if (params.id) {
-  //       const contactData = await getContact(params.id);
-
-  //       setContact(contactData);
-  //     }
-  //   };
-
-  //   fetchContact();
-  // }, [params.id]);
-
-  // if (!contact) {
-  //   return <div>Loading...</div>;
-  // }
 
   return (
     <div>
