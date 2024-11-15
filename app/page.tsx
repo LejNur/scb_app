373bfc8a997db43d77b9fbde2ee58eb2e9a81459
Lejla <lejla.nrk@gmail.com>
2024-11-15 09:22:21 +0100

    chore: base url

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import { getAllContacts } from "./actions/getAllContacts";
 import ContactsList from "@/components/Organism/ContactsList";
 
 export default async function Home() {
-  if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
-    return null;
-  }
   const contacts = await getAllContacts();
 
   return (
