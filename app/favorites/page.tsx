1f0d40a017a63812a93e5efa01475e2f4e339aac
Lejla <lejla.nrk@gmail.com>
2024-11-15 16:52:53 +0100

    export const dynamic = "force-dynamic";

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,5 +1,6 @@
 import ContactsList from "@/components/Organism/ContactsList";
 import { getFavorites } from "../actions/getFavorites";
+export const dynamic = "force-dynamic";
 
 export default async function Favorites() {
   const favorites = await getFavorites();
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ContactsList from "@/components/Organism/ContactsList";
 import { getAllContacts } from "./actions/getAllContacts";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const contacts = await getAllContacts();
