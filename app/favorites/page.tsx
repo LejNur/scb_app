407cd7c20d2e748addb2e39a10716180c90f8b7b
Lejla <lejla.nrk@gmail.com>
2024-11-15 02:13:21 +0100

    chore: last

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,5 @@
 import ContactsList from "@/components/Organism/ContactsList";
 import { getFavorites } from "../actions/getFavorites";
-export const dynamic = "force-dynamic";
 
 export default async function Favorites() {
   const favorites = await getFavorites();
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { getAllContacts } from "./actions/getAllContacts";
 import ContactsList from "@/components/Organism/ContactsList";
-export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const contacts = await getAllContacts();
