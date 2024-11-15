796d2f114c8c238a42ab06b9921a88578c9a745a
Lejla <lejla.nrk@gmail.com>
2024-11-15 09:18:15 +0100

    fix: env variable

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { getAllContacts } from "./actions/getAllContacts";
 import ContactsList from "@/components/Organism/ContactsList";
 
 export default async function Home() {
+  if (!process.env.NEXT_PUBLIC_API_BASE_URL) {
+    return null;
+  }
   const contacts = await getAllContacts();
 
   return (
